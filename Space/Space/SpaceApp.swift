8b0cb401f001abc3a0fc3d16053711d9fb507308
stephen-a-weber <32736986+stephen-a-weber@users.noreply.github.com>
2022-08-15 14:10:36 -0400

    Add files via upload

---
+++ Space/Space/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Space
+//
+//  Created by stephen weber on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
